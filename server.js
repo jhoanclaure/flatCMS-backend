e69c40b3e7e96aa7e32a8af7ef003a4c77e0a940
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2026-03-03 00:24:00 -0400

    Refactor: para deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.send("Servidor funcionando correctamente");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
